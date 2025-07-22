55b4702909440361797fb322faa54a9329015842
Prem Vaniya <113230199+premvaniya@users.noreply.github.com>
2025-07-22 21:50:48 +0530

    Update game_progress.gd

--- scripts/game_progress.gd
+++ scripts/game_progress.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var level_status = {
-	1: {"unlocked": true, "completed": false},  # First level starts unlocke
+	1: {"unlocked": true, "completed": false},  # First level starts unlockedd
 	2: {"unlocked": false, "completed": false},
 	3: {"unlocked": false, "completed": false},
 }
